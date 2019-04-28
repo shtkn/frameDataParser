b247bab5b41befdb6456a075ae4a3e9dae5bc322
shtkn <shtkn001@gmail.com>
2019-04-28 16:19:10 -0700

    initial checkin. Still working on calculating frame advantage

---
+++ createAnnotatedDataFiles.py
@@ -0,0 +1,73 @@
+import os, fnmatch
+
+attackHitboxFile = open("all_active.txt", "r")
+contents = attackHitboxFile.readlines()
+charDictionarySet = {}
+currentChar = ''
+currentSet = None
+for line in contents:
+    if line.endswith(" starts here\n"):
+        char = line.split(" ")[0]
+        print char
+        currentSet = set()
+        charDictionarySet[char] = currentSet
+    elif currentSet is None:
+        continue
+    else:
+        currentSet.add(line.strip())
+        # print line.strip()
+# print charDictionarySet.keys()
+
+outputDir = "./output/"
+inputDir = "./input/"
+
+fileList = os.listdir(inputDir)
+pattern = "*.py"
+# for entry in fileList:
+#     if fnmatch.fnmatch(entry, pattern):
+#         print entry
+#         print entry[entry.index("_")+1:entry.__len__()-3]
+
+for entry in fileList:
+    source = open(inputDir + entry, "r")
+    target = open(outputDir + entry, "w")
+    contents = source.readlines()
+    fileAbbr = entry[entry.index("_") + 1:entry.__len__() - 3]
+    if fileAbbr.__len__() > 3:
+        fileAbbr = fileAbbr[0:3]
+    if not charDictionarySet.__contains__(fileAbbr):
+        print "Couldn't find: " + fileAbbr
+        continue
+    strBuffer = ""
+    currentSet = charDictionarySet[fileAbbr]
+    SPRITE_START = "sprite('"
+    SPRITE_MID = "', "
+    SPRITE_END = ")"
+    prevFrameCount = 1
+    for line in contents:
+        if line.__contains__("@State"):
+            prevFrameCount = 1  # new move, restart frame counter
+        if line.__contains__(SPRITE_START):
+            # check if this sprite has a hitbox
+            # find it's duration
+            # print line
+            start = line.index(SPRITE_START) + SPRITE_START.__len__()
+            end = line.index(SPRITE_MID)
+            sprite = line[start: end]
+            # print sprite
+            numberStart = line.index(SPRITE_MID) + SPRITE_MID.__len__()
+            numberEnd = line.index(SPRITE_END)
+            duration = int(line[numberStart:numberEnd])
+            # print duration
+            newFrameCount = prevFrameCount+duration-1
+            line = line[0:line.__len__()-1] + "\t# " + str(prevFrameCount) + "-" + str(newFrameCount)
+            prevFrameCount = newFrameCount + 1
+            if currentSet.__contains__(sprite):
+                line = line[0:] + "\t **attackbox here**"
+
+            line += "\n"
+
+        # print line
+        target.write(line)
+
+print "DONE"
---
+++ parseAttacks.py
@@ -0,0 +1,189 @@
+sourceDir = "./"
+entry = "testfile"
+
+
+class State:
+    def __init__(self):
+        self.spriteLine = ""
+        self.extraLines = ""
+        self.duration = 0
+        self.isAttackBox = False
+        self.disableAttackboxes = False
+        self.disableAttackboxesThisFrame = False
+        self.isNewHit = True
+        self.blockstun = 0
+
+    def clear_values(self, is_new_move):
+        self.spriteLine = ""
+        self.extraLines = ""
+        self.duration = 0
+        self.isAttackBox = False
+        self.isNewHit = False
+        if is_new_move:
+            self.disableAttackboxes = False
+            self.isNewHit = True
+        self.disableAttackboxesThisFrame = False
+
+    def is_attackbox(self):
+        return self.isAttackBox and not (self.disableAttackboxes or self.disableAttackboxesThisFrame)
+
+
+class AbstractChunk:
+    def __init__(self, duration=0):
+        self.duration = duration
+
+    def __str__(self):
+        return str(self.duration)
+
+
+class AttackFrameChunk(AbstractChunk):
+    def __init__(self, duration=0, blockstun=0, is_attack_box=False, is_new_hit=False):
+        AbstractChunk.__init__(self, duration)
+        self.blockstun = blockstun
+        self.isAttackBox = is_attack_box
+        self.isNewHit = is_new_hit
+
+    def __str__(self):
+        toReturn = str(self.duration)
+        if self.isAttackBox:
+            toReturn += " Active"
+            toReturn += " New Hit " + str(self.isNewHit)
+        return toReturn
+
+
+class FrameChunk(AbstractChunk):
+    def __init__(self, duration=0):
+        AbstractChunk.__init__(self, duration)
+
+
+SPRITE_START = "sprite('"
+SPRITE_MID = "', "
+SPRITE_END = ")"
+HAS_HITBOX = "**attackbox here**"
+
+
+def get_duration(sprite_str):
+        number_start = sprite_str.index(SPRITE_MID) + SPRITE_MID.__len__()
+        number_end = sprite_str.index(SPRITE_END)
+        return int(line[number_start:number_end])
+
+
+def consolidate_frame_chunks(chunk_list):
+    new_chunk_list = []
+
+    prev_chunk = move[0]
+    for i in range(1, len(chunk_list)-1):
+        chunk = move[i]
+        if isinstance(prev_chunk, AttackFrameChunk) and isinstance(chunk, AttackFrameChunk):
+            if chunk.isNewHit:
+                new_chunk_list.append(prev_chunk)
+                prev_chunk = chunk
+            else:
+                prev_chunk.duration += chunk.duration
+        elif isinstance(prev_chunk, FrameChunk) and isinstance(chunk, FrameChunk):
+                prev_chunk.duration += chunk.duration
+        else:
+            new_chunk_list.append(prev_chunk)
+            prev_chunk = chunk
+    new_chunk_list.append(prev_chunk)
+    #
+    # for chunk in newChunkList:
+    #     print chunk
+    return new_chunk_list
+
+
+state = State()
+source = open(sourceDir + entry, "r")
+target = open(entry + ".txt", "w")
+contents = source.readlines()
+
+moveList = {}
+move = None
+moveName = ""
+for line in contents:
+    if "@State" in line:     # new move, finish parsing existing move, then restart frame counters
+        if move is not None and len(move) > 0:
+            chunk = AttackFrameChunk(state.duration, state.blockstun, state.is_attackbox(),
+                                     state.isNewHit) if state.is_attackbox() else FrameChunk(state.duration)
+            move.append(chunk)
+            moveList[moveName] = consolidate_frame_chunks(move)
+        move = []
+        moveName = ""
+        # print "*** NEW MOVE ***"
+        # restart counters
+        state.clear_values(True)
+
+    elif SPRITE_START in line:
+        if state.duration > 0:
+            chunk = AttackFrameChunk(state.duration, state.blockstun, state.is_attackbox(),
+                                     state.isNewHit) if state.is_attackbox() else FrameChunk(state.duration)
+            move.append(chunk)
+            state.clear_values(False)
+        spriteLine = line
+        state.duration = get_duration(spriteLine)
+
+        if HAS_HITBOX in spriteLine:
+            state.isAttackBox = HAS_HITBOX in spriteLine
+
+    else:
+        if "Recovery()" in line or "Unknown23027()" in line:  # disables active frames for rest of move
+            state.disableAttackboxes = True
+        elif "StartMultihit()" in line:  # turns off these active frames
+            state.disableAttackboxesThisFrame = True
+        elif "RefreshMultiHit()" in line:  # counts as a new hit, end previous frames; start a  new one
+            state.isNewHit = True
+        elif "AttackLevel_(" in line:  # get hitstun/blockstun values according to it's level
+            level = line[line.index("(")+1:line.index("(")+2]
+            # print "LEVEL: " + level
+            state.blockstun = 9 + int(level)*2
+            if int(level) >= 3:
+                state.blockstun += 1
+            # blockstun by level: 0: 9F, 1: 11F, 2: 13F, 3: 16F, 4: 18F, 5: 20F
+            # so blockstun = 0 * Level*2. If Level 3 or higher, blockstun + 1
+        elif "Unknown11028(" in line:
+            state.blockstun = line[line.index("(")+1:line.index(")")]
+            # print "Hardcoded blockstun: " + state.blockstun
+        elif "def " in line and len(moveName) < 1:
+            name_start = line.index("def ") + SPRITE_MID.__len__() +1
+            name_end = line.index("()")
+            moveName = line[name_start:name_end]
+
+
+if state.duration > 0:
+    chunk = AttackFrameChunk(state.duration, state.blockstun, state.is_attackbox(),
+                             state.isNewHit) if state.is_attackbox() else FrameChunk(state.duration)
+    move.append(chunk)
+    moveList[moveName] = consolidate_frame_chunks(move)
+
+for moveName in moveList:
+    move = moveList[moveName]
+    startup = 0
+    middle = ""
+    recovery = ""
+    print moveName
+
+    for idx, chunk in enumerate(move):
+        if idx == 0 and len(move) > 1:
+            startup = chunk.duration +1
+        else:
+            if isinstance(chunk, AttackFrameChunk):
+                if len(middle) > 0 and middle[len(middle)-1] != ")":
+                    middle += ","
+                middle += str(chunk.duration)
+            elif idx < len(move) -1:
+                middle += "(" + str(chunk.duration) + ")"
+            else:
+                recovery += str(chunk.duration)
+
+    print str(startup) + " " + middle + " " + recovery
+    # determine frame adv
+    total_duration = 0
+    for idx, chunk in enumerate(move):
+        total_duration += chunk.duration
+
+
+
+
+print "DONE"
+
+
