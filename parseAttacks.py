a74ed3a8b25f11f6b37247ca04a66d5c0f92c131
shtkn <shtkn001@gmail.com>
2019-12-07 16:49:19 -0800

    nevermind, just worry about GFX_0

--- parseAttacks.py
+++ parseAttacks.py
@@ -392,7 +392,7 @@ SPRITE_START = "    sprite('"
 SPRITE_MID = "', "
 SPRITE_END = ")"
 HAS_HITBOX = "**attackbox here**"
-GFX_START = "GFX_"
+GFX_START = "GFX_0"
 CALL_SUBROUTINE_STARTC = "CallSubroutine('"
 CALL_SUBROUTINE_STARTc = "callSubroutine('"
 
